e8dc4c0ae4ace7dc16b18ec334be51093ca5a3ea
Osaro Iyoha <saro.iyoha@gmail.com>
2020-04-15 09:56:33 +0100

    ES6: Destructuring, block Scoping Spread Operators

---
+++ 18. ES6 - Features that make your life easy/5. Destructuring - functions/code.js
@@ -0,0 +1,26 @@
+//ES5
+function f(arr){
+    console.log('hello ' + arr[0] + ' ' + arr[1]);
+}
+f(['Bill', 'Gates']);
+
+//ES6
+//parameter matching
+function g([firstname, lastname]){
+    console.log(`hello ${firstname} ${lastname} !`);
+}
+g(['Bill', 'Gates']);
+
+function h({firstname, lastname}){
+    console.log(`hello ${firstname} ${lastname} !`);
+}
+h({firstname: 'Mark', lastname: 'Zuckerberg'});
+
+var arrd = ["Osaro","Iyoha"];
+function x([one, two]){
+    console.log(one, two);
+}
+x(arrd);
+
+let add = x => x+1
+console.log(add(1));
